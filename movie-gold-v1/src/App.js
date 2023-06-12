47f917f4730341ee2828875871ecac424e33b187
ipekozbay <20060401@stu.omu.edu.tr>
2023-06-12 15:11:02 +0300

    created home component

--- movie-gold-v1/src/App.js
+++ movie-gold-v1/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import api from './api/axiosConfig'
 import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
 
     try {
       const response = await api.get("/api/v1/movies");
- 
+
       setMovies(response.data);
 
     } catch (err) {
@@ -22,12 +23,16 @@ function App() {
   }
 
   useEffect(() => {
-      getMovies();
-  },[])
+    getMovies();
+  }, [])
 
   return (
     <div className="App">
-      
+      <Route path='/' element={<Layout/>}>
+        <Route path='/' element={<Home />}>
+
+        </Route>
+      </Route>
     </div>
   );
 }
