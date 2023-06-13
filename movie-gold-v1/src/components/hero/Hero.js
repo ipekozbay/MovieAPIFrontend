c95e6ba2cf858266a3a0d42b2cbe129197e9f69d
ipekozbay <20060401@stu.omu.edu.tr>
2023-06-13 21:52:57 +0300

    13.06.2023

---
+++ movie-gold-v1/src/components/hero/Hero.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import '/.Hero.css';
+import Carousel from 'react-material-ui-carousel';
+import { Paper } from '@mui/material';
+
+const Hero = ({ movies }) => {
+    return (
+        <Carousel>
+            {movies.map((movie) => {
+                return(
+                    <Paper>
+                        <div className='movie-card-container'>
+                            <div className='movie-card'>
+                                <div className='movie-poster'>
+                                    <img src='movie.poster' />
+                                </div>
+                                <div className='movie-title'>
+                                    <h4>{movie.title}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </Paper>
+                )
+            })
+
+            }
+
+        </Carousel>
+    )
+
+}
+
+export default Hero;
