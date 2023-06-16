17398a117ae19677b102534661bcfd70ac885e70
ipekozbay <20060401@stu.omu.edu.tr>
2023-06-16 22:55:04 +0300

    16.06.2023

--- movie-gold-v1/src/components/hero/Hero.js
+++ movie-gold-v1/src/components/hero/Hero.js
@@ -10,7 +10,7 @@ const Hero = ({ movies }) => {
                 return(
                     <Paper>
                         <div className='movie-card-container'>
-                            <div className='movie-card'>
+                            <div className='movie-card' style={{"--img":`url(${movie.backdrops[0]})`}}>
                                 <div className='movie-poster'>
                                     <img src='movie.poster' />
                                 </div>
