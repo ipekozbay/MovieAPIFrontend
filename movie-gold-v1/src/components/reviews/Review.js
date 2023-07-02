d69facc34d728ce95533122174acb47d2e4378f4
ipekozbay <20060401@stu.omu.edu.tr>
2023-07-02 23:56:00 +0300

    02.11.2023

---
+++ movie-gold-v1/src/components/reviews/Review.js
@@ -0,0 +1,20 @@
+import {useEffect, useRef} from 'react';
+import api from '../../api/axiosConfig';
+import {useParams} from 'react-router-dom';
+import {Container, Row, Col} from 'react-bootstrap';
+import ReviewForm from '../reviewForm/ReviewForm';
+
+import React from 'react'
+
+
+import React from 'react'
+
+const Review = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Review
