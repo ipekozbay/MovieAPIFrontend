518d9b57a8bdd1a3445a868c9a65313a81a34803
ipekozbay <20060401@stu.omu.edu.tr>
2023-07-04 00:13:37 +0300

    04.07.2023

--- movie-gold-v1/src/components/reviews/Review.js
+++ movie-gold-v1/src/components/reviews/Review.js
@@ -1,7 +1,7 @@
-import {useEffect, useRef} from 'react';
+import { useEffect, useRef } from 'react';
 import api from '../../api/axiosConfig';
-import {useParams} from 'react-router-dom';
-import {Container, Row, Col} from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 import ReviewForm from '../reviewForm/ReviewForm';
 
 import React from 'react'
@@ -9,28 +9,44 @@ import React from 'react'
 
 import React from 'react'
 
-const Review = ({getMovieData, movie, reviews, setReviews}) => {
+const Review = ({ getMovieData, movie, reviews, setReviews }) => {
 
     const revText = useRef();
-    let params =useParams();
+    let params = useParams();
     const movieId = params.movieId;
 
     useEffect(() => {
         getMovieData(movieId);
-    },[])
+    }, [])
 
-  return (
-    <div>
-        <Row>
-            <Col><h3>Reviews</h3></Col>
-        </Row>
-        <Row className='mt-2'>
-            <Col>
-                <img src='' alt=''/>
-            </Col>
-        </Row>
-    </div>
-  )
+    return (
+        <div>
+            <Row>
+                <Col><h3>Reviews</h3></Col>
+            </Row>
+            <Row className='mt-2'>
+                <Col>
+                    <img src={movie?.poster} alt='' />
+                </Col>
+                <Col>
+                    {
+                        <>
+                            <Row>
+                                <Col>
+                                    <ReviewForm handleSubmit={addReview} revText={revText} labelText="write a review?" />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <hr />
+                                </Col>
+                            </Row>
+                        </>
+                    }
+                </Col>
+            </Row>
+        </div>
+    )
 }
 
 export default Review
